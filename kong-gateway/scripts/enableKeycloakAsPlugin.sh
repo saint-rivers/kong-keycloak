9414dffb82672d6e5ba67fba2fc964fc7de7a38c
Dayan Eam <eam.dayan@gmail.com>
2022-06-03 14:28:45 +0700

    change discovery url

--- kong-gateway/scripts/enableKeycloakAsPlugin.sh
+++ kong-gateway/scripts/enableKeycloakAsPlugin.sh
@@ -2,15 +2,15 @@
 
 # curl -X DELETE 'http://localhost:8001/services/test-api/plugins/'
 
+export HOST="http://saintrivers.tech:8800/auth/realms/myrealm/.well-known/openid-configuration"
+
 curl -X POST \
   --url http://localhost:8001/services/test-api/plugins \
   --data "name=oidc" \
   --data "config.client_id=myclient" \
   --data "config.client_secret=0VvCNsz560uplf6nQm5y9jMsm1ZpbaF6" \
-  --data "config.discovery=https://128.199.164.171:9443/auth/realms/myrealm/.well-known/openid-configuration" \
+  --data "config.discovery=$HOST" \
   --data "config.realm=myrealm"
-
-  # --data "config.discovery=http://128.199.164.171:8800/auth/realms/myrealm/.well-known/openid-configuration" \
 
 # this redirects properly but google cannot redirect to a private ip address
 # 172.26.0.5
