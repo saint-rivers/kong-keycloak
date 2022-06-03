e7c5ecf0d342010965a427de2bd24d18a02f784f
Dayan Eam <eam.dayan@gmail.com>
2022-06-03 16:22:00 +0700

    update

--- test-service/src/main/java/com/ksga/test/config/WebSecurityConfig.java
+++ test-service/src/main/java/com/ksga/test/config/WebSecurityConfig.java
@@ -11,23 +11,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // http
-        // .cors().and()
-
-        // .authorizeRequests()
-        // .antMatchers("/api/test/unsecured").permitAll()
-
-        // .anyRequest().authenticated()
-
-        // .and()
-        // .oauth2ResourceServer()
-        // .jwt()
-        // ;
-
         http
-                .authorizeRequests(auth -> auth.antMatchers("/api/test/unsecured")
-                        .hasAuthority("SCOPE_read")
-                        .anyRequest().authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt());
+                .cors().and()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/test/unsecured").permitAll()
+
+                .anyRequest().authenticated()
+
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+
+        // http
+        // .authorizeRequests(auth -> auth.antMatchers("/api/test/unsecured")
+        // .hasAuthority("SCOPE_read")
+        // .anyRequest().authenticated())
+        // .oauth2ResourceServer(oauth2 -> oauth2.jwt());
     }
 }
